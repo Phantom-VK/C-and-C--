683a22928a348805d2ba810eb07b746779215641
Vikramaditya G Khupse <vikramadityakhupse@gmail.com>
2023-08-27 12:37:57 +0530

    Geek for geeks top 50 array questions Q. duplicates in array

---
+++ duplicates_in_array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> duplicates(int arr[], int n) {
+        unordered_map<int, int> freqMap;
+        vector<int> result;
+
+        for (int i = 0; i < n; i++) {
+            freqMap[arr[i]]++;
+        }
+
+        for (auto it : freqMap) {
+            if (it.second > 1) {
+                result.push_back(it.first);
+            }
+        }
+
+        if (result.empty()) {
+            result.push_back(-1);
+        }
+
+        sort(result.begin(), result.end());
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int arr[] = {4, 3, 2, 7, 8, 2, 1, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    vector<int> result = sol.duplicates(arr, n);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
