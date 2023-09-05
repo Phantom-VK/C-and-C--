1a495e248609e5f38ed6f968e15b3a5a34cc89c6
Phantom-VK <vikramadityakhupse@gmail.com>
2023-09-05 12:48:39 +0530

    some new codes

---
+++ merge_array.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void merge(int a[], int n, int b[], int m) {
+    
+
+    const int size = n + m;//max size of array c
+    int c[size];//declaring array c with max size
+    int k = 0;//size of array c which will increase as increase in elements
+
+    for (int i = 0; i < n; i++) {
+        int found = 0; //flag for if an element is already in array
+        //here first loop is for taking each element of array 1
+        for (int j = 0; j < k; j++) {
+            //second loop is for taking element of array c i.e. our output array
+            if (a[i] == c[j]) {
+                
+                //if an element is already in given array c 
+                //then found == 1 and we wil break loop and pass on nect if condition
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            //here if a[i]
+            c[k++] = a[i];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int found = 0; 
+
+        for (int j = 0; j < k; j++) {
+            if (b[i] == c[j]) {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            c[k++] = b[i];
+        }
+    }
+    sortArray(c, k);
+    for (int i = 0; i < k; i++) {
+        printf("%d ", c[i]);
+    }
+}
+
+
+int main() {
+
+    
+    int array1[100];
+    int array2[100];
+    int size1, size2;
+    printf("Enter size of array1: ");
+    scanf("%d", &size1);
+    printf("Enter %d elements in array1: ", size1);
+    for (int i = 0; i < size1; i++)
+    {
+        scanf("%d", &array1[i]);
+    }
+
+    printf("Enter size of array2: ");
+    scanf("%d", &size2);
+    printf("Enter %d elements in array2: ", size2);
+    for (int i = 0; i < size2; i++)
+    {
+        scanf("%d", &array2[i]);
+    }
+    
+
+    //calling merge function
+    merge(array1, size1, array2, size2);
+    return 0;
+}
--- perefect_square_number.c
+++ perefect_square_number.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+//Function declaration 
 int isPerfectSquare(int n);
 
+//Function definition
 int isPerfectSquare(int n) {
+    //we are runing for loop from 1 until n i.e given number
     for (int i = 1; i * i <= n; i++) {
+        
         if ((n % i == 0) && (n / i == i)) {
             return 1; 
         }
@@ -13,12 +17,17 @@ int isPerfectSquare(int n) {
 
 int main() {
     long long int n;
+    //here we intialized a variable n of type long long int and taking inpu
+    //in it.
     printf("Enter a number: ");
     scanf("%lld", &n);
-
+    //we are calling function in if condition and function is returning 0 or 1
+    //that is false or true.
     if (isPerfectSquare(n)) {
+        //So if true
         printf("Yes, it is a perfect square number.\n");
     } else {
+        //else
         printf("No, it is not a perfect square number.\n");
     }
 
