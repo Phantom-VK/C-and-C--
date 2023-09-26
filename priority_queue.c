73e49bec0d33807a067317218f604881950e7251
Phantom-VK <vikramadityakhupse@gmail.com>
2023-09-26 16:29:40 +0530

    26 sep 2023

---
+++ priority_queue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define MAX_SIZE 100;
+
+struct item{
+
+    int value;
+    int priority;
+
+}
+
+struct item pr[MAX_SIZE];
+int size = -1;
+
+void enqueue(int item, int priority){
+
+    int highestPriority = 0;
+    int index = -1;
+}
+
---
+++ temp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+
+    int num1, num2;
+    printf("Enter number 1: \n");
+    scanf("%d", &num1);
+
+    printf("Enter number 1: ");
+    scanf("%d", &num2);
+
+    printf("Addition of two numbers is : %d", num1+num2);
+}
